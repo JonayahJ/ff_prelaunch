62572dd7278c700df760d78ff2e68a7650dd2dd6
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-05-27 09:29:55 -0400

    added page routes

--- fourfront_prelaunch/views.py
+++ fourfront_prelaunch/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def dynamic_pages(request, page):
+    page_text = None
+    if page == "home":
+        page_text = "This works!"
+    elif page == "survey":
+        page_text = "This is the questionnaire!"
+    else:
+        return HttpResponseNotFound("This page is not supported.")
+    return HttpResponse(page_text)
