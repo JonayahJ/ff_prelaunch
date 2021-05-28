74cec1b1381dd57e7e673cbae82993a1166cfe7d
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-05-27 21:39:24 -0400

    installed node-sass

--- fourfront/settings.py
+++ fourfront/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1kg**8=sph2qr0+z*9nr6i*($4+rx(d=th!m9)#d2rxvsooh0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
